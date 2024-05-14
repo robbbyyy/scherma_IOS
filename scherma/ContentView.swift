832780df3f0c6a9c26905db949776e3704ae5155
robbbyyy <roberto.dellacorte001@studenti.uniparthenope.it>
2024-05-14 18:40:10 +0200

    fix behaviour

--- scherma/ContentView.swift
+++ scherma/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "moon")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("simm e chiù fort!!")
--- scherma/ViewController.swift
+++ scherma/ViewController.swift
@@ -8,24 +8,5 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        startAccelerometerUpdates()
-    }
-    
-    func startAccelerometerUpdates() {
-        if manager.isAccelerometerAvailable {
-            manager.accelerometerUpdateInterval = 0.1  // Imposta l'intervallo di aggiornamento dell'accelerometro
-            
-            manager.startAccelerometerUpdates(to: .main) { (data, error) in
-                guard let acceleration = data?.acceleration else {
-                    print("Errore nel rilevamento dell'accelerazione: \(error?.localizedDescription ?? "Errore sconosciuto")")
-                    return
-                }
-                
-                // Stampiamo i dati di accelerazione sulla console
-                print("X: \(acceleration.x), Y: \(acceleration.y), Z: \(acceleration.z)")
-            }
-        } else {
-            print("L'accelerometro non è disponibile.")
-        }
     }
 }
